e18d8ae18909e84c2722e1a3b2d1af482f7caf08
lionely <lionelscotty95@gmail.com>
2018-02-07 19:47:13 -0600

    Using wav as sound for dance

--- SturdyDance.py
+++ SturdyDance.py
@@ -1,5 +1,6 @@
 from SturdyRobot import *
 import time as t
+
 robot = SturdyRobot("A")
 ev3.Sound.set_volume(10)
 
@@ -9,27 +10,28 @@ starSong = [('C4', 'q'), ('C4', 'q'), ('G4', 'q'), ('G4', 'q'),
             ('D4', 'q'), ('D4', 'q'), ('C4', 'h') ]
 
 
-#time.sleep(1.0)
 
-def playSong(song):
-    ev3.Sound.play_song(song)
 
+def playSong():
+    ev3.Sound.play('robot_jazz.wav')
 
 def dance(robot,speed,time=None):
-    playSong(starSong)
-    #robot.curve(speed,2,1)#trying to go in s curve
-    t.sleep(0.5)
-    #robot.curve(2,speed,1)
-    t.sleep(0.5)
-    robot.forward(speed,time)# go onto the stage
-    t.sleep(1.0)
-    robot.turnLeft(0.5,2.5)#spin around show your stuff!
-    t.sleep(0.5)
-    robot.turnRight(0.5,2.5)
-    t.sleep(0.5)
-    robot.backward(speed,time)
-    t.sleep(0.5)
-    robot.pointerLeft(speed,2.5)
-#ev3.Sound.speak("I love goats!").wait()
-#time.sleep(1.0)
+    begin = t.time()
+    playSong()
+    while t.time - begin < 30:
+        robot.curve(speed,speed/2,1)#trying to go in s curve
+        t.sleep(0.5)
+        robot.curve(speed/2,speed,1)
+        t.sleep(0.5)
+        robot.forward(speed,time)# go onto the stage
+        t.sleep(1.0)
+        robot.turnLeft(0.5,2.5)#spin around show your stuff!
+        t.sleep(0.5)
+        robot.turnRight(0.5,2.5)
+        t.sleep(0.5)
+        robot.backward(speed,time)
+        t.sleep(0.5)
+        robot.pointerLeft(speed,2.5)
+
+        
 dance(robot,1,1)
