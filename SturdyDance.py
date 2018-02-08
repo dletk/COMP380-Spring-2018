f25ff8e2c5e057286721ac70af18ea5df223775d
lionely <lionelscotty95@gmail.com>
2018-02-07 20:03:00 -0600

    Dance changes.

--- SturdyDance.py
+++ SturdyDance.py
@@ -4,14 +4,6 @@ import time as t
 robot = SturdyRobot("A")
 ev3.Sound.set_volume(100)
 
-starSong = [('C4', 'q'), ('C4', 'q'), ('G4', 'q'), ('G4', 'q'),
-            ('A4', 'q'), ('A4', 'q'), ('G4', 'h'),
-            ('F4', 'q'), ('F4', 'q'), ('E4', 'q'), ('E4', 'q'),
-            ('D4', 'q'), ('D4', 'q'), ('C4', 'h') ]
-
-
-
-
 def playSong():
     ev3.Sound.play('robot_jazz.wav')
 
@@ -20,18 +12,19 @@ def dance(robot,speed,time=None):
     playSong()
     while t.time() - begin < 30:
         robot.curve(speed,speed/2,1)#trying to go in s curve
-        t.sleep(0.5)
+        t.sleep(0.2)
         robot.curve(speed/2,speed,1)
-        t.sleep(0.5)
+        t.sleep(0.2)
         robot.forward(speed,time)# go onto the stage
         t.sleep(1.0)
         robot.turnLeft(0.5,2.5)#spin around show your stuff!
-        t.sleep(0.5)
+        t.sleep(0.2)
         robot.turnRight(0.5,2.5)
-        t.sleep(0.5)
-        robot.backward(speed,time)
-        t.sleep(0.5)
+        t.sleep(0.2)
+        robot.backward(speed,1.5*time)
+        t.sleep(0.1)
         robot.pointerLeft(speed,2.5)
-
+        t.sleep(0.1)
+        robot.turnRight(0.5,3)
 
 dance(robot,1,1)
