0909502c0e9c1d9945cf4412bfffa99480e97b91
dletk <duc.mtle@gmail.com>
2018-02-27 20:23:10 -0600

    Fixed the bug in followingWall, hw2_task3, the condition for going too left is <=350, not >=350

--- hw2_task3.py
+++ hw2_task3.py
@@ -1,29 +1,32 @@
 from SturdyRobot import *
 
 
-
 def followWall():
-    #robot.pointerTo(90)
-    while robot.readDistance()<20:
-        robot.forward(0.2,0.5)
-        if robot.readDistance()<15:
+    """Making the robot to follow a wall of the maze."""
+    # robot.pointerTo(90)
+    distance_to_wall = robot.readDistance()
+    while distance_to_wall < 20:
+        robot.forward(0.2, 0.5)
+        print("====> Distance: ", distance_to_wall)
+        if distance_to_wall < 15:
             robot.stop()
-            robot.turnLeft(0.3,0.1)
-            robot.forward(0.3,0.2)
-            #too much to right
-            robo_heading = robot.readHeading()
-            if robo_heading<=90 and robo_heading>10:
-                while robo_heading<= 10:
-                    print("Heading too right: ",robo_heading)
-                    robot.turnLeft(0.1,0.2)
-                    robo_heading = robot.readHeading()
-            #
-            elif robo_heading >=270 and robo_heading<350:
-                while robo_heading>=350:
-                    print("Heading too left: ",robo_heading)
+            robot.turnLeft(0.3, 0.1)
+            robot.forward(0.3, 0.2)
+            # too much to right
+            robot_heading = robot.readHeading()
+            if robot_heading <= 90 and robot_heading > 10:
+                while robot_heading <= 10:
+                    print("Heading too right: ", robot_heading)
+                    robot.turnLeft(0.1, 0.2)
+                    robot_heading = robot.readHeading()
+            # too much to left
+            elif robot_heading >= 270 and robot_heading < 350:
+                while robot_heading <= 350:
+                    print("Heading too left: ", robot_heading)
 
-                    robot.turnRight(0.1,0.2)
-                    robo_heading = robot.readHeading()
+                    robot.turnRight(0.1, 0.2)
+                    robot_heading = robot.readHeading()
+        distance_to_wall = robot.readDistance()
 
 
 robot = SturdyRobot('A')
