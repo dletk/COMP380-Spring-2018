e6069c79861438086529bc5cd3deb04345645bc3
dletk <duc.mtle@gmail.com>
2018-03-01 02:25:16 -0600

    Added completed hw2_task3 with touch sensor. Not tested on real maze

--- hw2_task3.py
+++ hw2_task3.py
@@ -16,32 +16,60 @@ from SturdyRobot import *
 # X  = 0.5,0.5
 
 SPEED_MOVEMENT = 0.05
-TIME_MOVE = 2
+TIME_MOVE = 2.0
+
+
+def foundTouch():
+    touch = robot.readTouch()
+    if touch == (0, 0):
+        return (False, None)
+    elif touch == (1, 1):
+        return (True, touch)
+    else:
+        # Repeat the touch 5 times so we can be sure which side is touched
+        for i in range(2):
+            robot.backward(SPEED_MOVEMENT, TIME_MOVE / 2)
+            robot.forward(SPEED_MOVEMENT, TIME_MOVE / 1.6)
+            touch = robot.readTouch()
+            if touch == (1, 1):
+                return (True, touch)
+        return (True, touch)
 
 
 def followWall():
-    corner = False
+    rightTurn = False
     foundColor = False
     while not foundColor:
         robot.forward(SPEED_MOVEMENT)
-        while robot.readTouch() != (1, 1):
-            pass
+        touch = foundTouch()
+        while not touch[0]:
+            touch = foundTouch()
+        touch = touch[1]
+        # # TODO: Here, try to deal with turn based on touch
+        print("Touch 1: ", touch)
         robot.stop()
         if robot.readColor() == 6:
             return True
         robot.backward(SPEED_MOVEMENT, TIME_MOVE)
-        if corner:
+        robot.turnExact90()
+        if rightTurn:
             robot.turnExact90()
-            robot.turnExact90()
-            # Here, we already moved to the direction of new wall, so reset corner
-            corner = False
-        else:
-            robot.turnExact90()
+            # Here, we already moved to the direction of new wall, so reset rightTurn
+            rightTurn = False
+        elif touch == (1, 0):
+            # There is a left turn
+            robot.forward(SPEED_MOVEMENT, time=4 * TIME_MOVE)
+            robot.turnExact90(side="left")
+            robot.forward(SPEED_MOVEMENT, time=2 * TIME_MOVE)
+
+        # Move forward and detect a right turn
         robot.forward(SPEED_MOVEMENT, 2 * TIME_MOVE)
-        if robot.readTouch() == (1, 1):
+        touch = foundTouch()[1]
+        print("Touch 2: ", touch)
+        if touch == (1, 1):
             if robot.readColor() == 6:
                 return True
-            corner = True
+            rightTurn = True
         robot.backward(SPEED_MOVEMENT, TIME_MOVE)
         robot.turnExact90(side="left")
 
