969f454fa7323b90c0e2487fbb52832ae3791b23
dletk <duc.mtle@gmail.com>
2018-02-28 21:46:13 -0600

    Working on hw2_task3

--- hw2_task3.py
+++ hw2_task3.py
@@ -4,36 +4,57 @@ from SturdyRobot import *
 def followWall():
     """Making the robot to follow a wall of the maze."""
     # robot.pointerTo(90)
+    following_wall = True
     distance_to_wall = robot.readDistance()
-    while robot.readTouch()[0] != 1:
-        robot.forward(0.2, 0.5)
-        print("====> Distance: ", distance_to_wall)
-        if distance_to_wall < 15:
-            robot.stop()
-            robot.turnLeft(0.3, 0.1)
-            robot.forward(0.3, 0.2)
-            # too much to right
-            robot_heading = robot.readHeading()
-            if robot_heading <= 90 and robot_heading > 10:
-                while robot_heading <= 10:
-                    print("Heading too right: ", robot_heading)
-                    robot.turnLeft(0.1, 0.2)
-                    robot_heading = robot.readHeading()
-            # too much to left
-            elif robot_heading >= 270 and robot_heading < 350:
-                while robot_heading <= 350:
-                    print("Heading too left: ", robot_heading)
+    # while robot.readTouch()[0] != 1:
+    # robot.forward(0.2, 0.5)
+    print("====> Distance: ", distance_to_wall)
+    if distance_to_wall > 20:
+        # There is a right turn
+        robot.forward(0.5, 0.4)
+        robot.turnExact90()
+        robot.forward(0.5, 0.3)
+        following_wall = False
+    # Too close to wall
+    elif distance_to_wall > 5:
+        # Too far away from the wall
+        robot.stop()
+        robot.turnRight(0.3, 0.1)
+        robot.forward(0.3, 0.2)
+        following_wall = True
+    elif distance_to_wall < 15:
+        robot.stop()
+        robot.turnLeft(0.3, 0.1)
+        robot.forward(0.3, 0.2)
+        following_wall = True
 
-                    robot.turnRight(0.1, 0.2)
-                    robot_heading = robot.readHeading()
-        distance_to_wall = robot.readDistance()
-    # Check whether there is a turn or just a go too far
-    # if distance_to_wall > 25:
-        # There is a turn
-    robot.backward(0.2,0.3)
-    robot.turnExact90()
-    robot.forward(0.5, 1)
+    if following_wall:
+        robot_heading = robot.readHeading()
+        print("Current heading: ", robot_heading)
+        # too much to right
+        if robot_heading <= 90 and robot_heading > 3:
+            while robot_heading >= 3:
+                # print("Heading too right: ", robot_heading)
+                robot.turnLeft(0.05, 0.1)
+                robot_heading = robot.readHeading()
+        # too much to left
+        elif robot_heading >= 270 and robot_heading < 357:
+            while robot_heading <= 357:
+                # print("Heading too left: ", robot_heading)
+                robot.turnRight(0.05, 0.1)
+                robot_heading = robot.readHeading()
+        robot.forward(0.4, 0.2)
+        if robot.readColor() == 6:
+            ev3.Sound.beep()
+    distance_to_wall = robot.readDistance()
+
+    # There is left turn
+
+    # robot.backward(0.2,0.3)
+
+    # TODO: Check if there is a white color
 
 
 robot = SturdyRobot('A')
-followWall()
+while True:
+    followWall()
