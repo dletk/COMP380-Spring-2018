e343536f7ff3707ce2753507c4f15b1e610a40a2
dletk <duc.mtle@gmail.com>
2018-03-01 21:41:39 -0600

    Comments added for hw2_task3

--- hw2_task3.py
+++ hw2_task3.py
@@ -1,63 +1,57 @@
 from SturdyRobot import *
 
-
-# New approach:
-# Corner = false
-# While color is not found:
-# go forward until both touch sensor is 1
-# Check if there is the matched color
-# Backward by a distance X
-# If corner then turn 180 else turn 90 to right
-# Go forward by 2X
-# if there is touch, check color, and remember corner = true
-# Go backward by X
-# Turn left
-
-# X  = 0.5,0.5
-
 SPEED_MOVEMENT = 0.05
 TIME_MOVE = 2.0
 
 
 def foundTouch():
+    """Method to indentify which touch whether the robot touches a wall, and which touched was that."""
     touch = robot.readTouch()
     if touch == (0, 0):
         return (False, None)
     elif touch == (1, 1):
         return (True, touch)
     else:
-        # Repeat the touch 5 times so we can be sure which side is touched
+        # Repeat the touch 2 times so we can be sure which side is touched
         for i in range(2):
             robot.backward(SPEED_MOVEMENT, TIME_MOVE / 2)
             robot.forward(SPEED_MOVEMENT, TIME_MOVE)
             touch = robot.readTouch()
+            # Give priority to the case where both sides are touched -> front wall
             if touch == (1, 1):
                 return (True, touch)
         return (True, touch)
 
 
 def followWall():
+    """Method to follow the wall of a maze until the target color WHITE was founded."""
     rightTurn = False
     foundColor = False
     while not foundColor:
+        # Go forward until there is a wall found
         robot.forward(SPEED_MOVEMENT)
         touch = foundTouch()
         while not touch[0]:
             touch = foundTouch()
         touch = touch[1]
-        # # TODO: Here, try to deal with turn based on touch
         print("Touch 1: ", touch)
+
+        # Check color of the current wall
         robot.stop()
         if robot.readColor() == 6:
             return True
+
+        # Move backward and turn to process to new area
         robot.backward(SPEED_MOVEMENT, TIME_MOVE)
         robot.turnExact90()
+
+        # If there is a right turn at this position detected earlier, make a right turn
         if rightTurn:
             robot.turnExact90()
             # Here, we already moved to the direction of new wall, so reset rightTurn
             rightTurn = False
         elif touch == (1, 0):
-            # There is a left turn
+            # There is a left turn at this position
             robot.forward(SPEED_MOVEMENT, time=4 * TIME_MOVE)
             robot.turnExact90(side="left")
             robot.forward(SPEED_MOVEMENT, time=2 * TIME_MOVE)
@@ -66,10 +60,15 @@ def followWall():
         robot.forward(SPEED_MOVEMENT, 2 * TIME_MOVE)
         touch = foundTouch()[1]
         print("Touch 2: ", touch)
+
+        # There is a right turn
         if touch == (1, 1):
             if robot.readColor() == 6:
                 return True
             rightTurn = True
+
+        # Move backward by half the distance moved earlier, and turn 90 degree
+        # to check that wall
         robot.backward(SPEED_MOVEMENT, TIME_MOVE)
         robot.turnExact90(side="left")
 
