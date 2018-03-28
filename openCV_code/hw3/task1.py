50586ccf755540331dd3518e9a3f3fbdca6f9c21
dletk <duc.mtle@gmail.com>
2018-03-27 23:06:16 -0500

    Using erode

--- openCV_code/hw3/task1.py
+++ openCV_code/hw3/task1.py
@@ -27,44 +27,45 @@ if __name__ == '__main__':
             difference_image, cv2.MORPH_BLACKHAT, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
 
         enhanced_edges_image = cv2.GaussianBlur(
-            enhanced_edges_image, (9, 9), 0)
+            enhanced_edges_image, (5, 5), 0)
 
         gray_image = cv2.cvtColor(enhanced_edges_image, cv2.COLOR_BGR2GRAY)
 
         ret, thresh = cv2.threshold(gray_image, 5, 255, cv2.THRESH_BINARY)
 
+        # Erosion to eliminate too small contours
+        erode_kernel = (3,3)
+        eroded_image = cv2.erode(thresh, erode_kernel, iterations=5)
         # Dilation to connect the close contor
         # Loop several time
-        dilated_kernel = (50,50)
-        dilated_image = cv2.dilate(thresh, dilated_kernel)
-        for i in range(10):
-            dilated_image = cv2.dilate(dilated_image, dilated_kernel)
+        dilated_kernel = (500, 500)
+        # dilated_image = cv2.dilate(thresh, dilated_kernel)
+        # for i in range(20):
+        dilated_image = cv2.dilate(eroded_image, dilated_kernel, iterations=20)
 
         image, contours, hier = cv2.findContours(
             dilated_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         print("===> VIDEO CONTOURS: ", len(contours))
-        result_image = cv2.drawContours(
-            dilated_image, contours, -1, (0, 255, 0))
+        # result_image = cv2.drawContours(
+        #     frame2, contours, -1, (0, 255, 0))
 
         # Drawing the rectanle around the contours
         for contour in contours:
             (x, y, width, height) = cv2.boundingRect(contour)
             # print("====> RECT: ", (x, y, width, height))
-            cv2.rectangle(result_image, (x, y),
+            cv2.rectangle(frame2, (x, y),
                           (x + width, y + height), (255, 0, 0), 2)
 
-        cv2.imshow("Video", result_image)
+        cv2.imshow("Video", frame2)
 
         # Create an image for quick comparison
-        image, contours, hier = cv2.findContours(
-            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        compare_image = cv2.drawContours(
-            thresh, contours, -1, (0, 255, 0))
-        cv2.imshow("Compare", compare_image)
-        print("COMPARE CONTOURS: ", len(contours))
-
-
+        # image, contours, hier = cv2.findContours(
+        #     thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #
+        # compare_image = cv2.drawContours(
+        #     thresh, contours, -1, (0, 255, 0))
+        # cv2.imshow("Compare", compare_image)
+        # print("COMPARE CONTOURS: ", len(contours))
 
         key_stroke = chr(cv2.waitKey(1) & 0xff)
 
