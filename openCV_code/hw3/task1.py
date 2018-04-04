3a141df442f6aff5d974732ef0d87ca10d6cd0c4
dletk <duc.mtle@gmail.com>
2018-04-04 16:59:11 -0500

    Finished HW3 with comments

--- openCV_code/hw3/task1.py
+++ openCV_code/hw3/task1.py
@@ -8,32 +8,37 @@ if __name__ == '__main__':
     # Using the external webcam
     videoCap = cv2.VideoCapture(1)
 
+    # Capture 2 continuous frames from the webcam to measure differences
     ret, frame = videoCap.read()
     ret, frame2 = videoCap.read()
 
     while ret:
+        # Calculate the differences between 2 frames
         difference_image = calculateAbsDiff(frame, frame2)
+        frame = frame2.copy()
 
         # Enhanced the edges in the images
         enhanced_edges_image = cv2.morphologyEx(
             difference_image, cv2.MORPH_BLACKHAT, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
 
+        # Blur out some negligible details
         enhanced_edges_image = cv2.GaussianBlur(
             enhanced_edges_image, (5, 5), 0)
 
+        # Convert the image to proper format for finding contours
         gray_image = cv2.cvtColor(enhanced_edges_image, cv2.COLOR_BGR2GRAY)
-
         ret, thresh = cv2.threshold(gray_image, 5, 255, cv2.THRESH_BINARY)
 
 
-        # Dilation eliminate too small contours
-        # Loop several time
+        # Dilation and erosion to eliminate too small contours and connect related
+        # contours together. Interate several times to strengthen the effect.
         dilated_kernel = (10, 10)
         dilated_image = cv2.dilate(thresh, dilated_kernel, iterations=10)
 
         erode_kernel = (5,5)
         eroded_image = cv2.erode(dilated_image, erode_kernel, iterations=10)
 
+        # Find all contours in the image
         image, contours, hier = cv2.findContours(
             eroded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -51,9 +56,8 @@ if __name__ == '__main__':
         cv2.imshow("Video", frame2)
 
         key_stroke = chr(cv2.waitKey(1) & 0xff)
-
         if key_stroke == "q":
             break
         else:
-            ret, frame = videoCap.read()
+            # ret, frame = videoCap.read()
             ret, frame2 = videoCap.read()
--- openCV_code/hw3/task3_2.py
+++ openCV_code/hw3/task3_2.py
@@ -3,13 +3,9 @@ import numpy as np
 
 default = input("Enter D to use default value or M to enter manually: ")
 if default in "mM":
-    hueLower = input("Enter the lower bound for HUE: ")
-    hueUpper = input("Enter the upper bound for HUE: ")
-    # (rLower, gLower, bLower) = [int(x) for x in  rgbLower.split("-")]
-    # (rUpper, gUpper, bUpper) = [int(x) for x in  rgbUpper.split("-")]
+    hueLower = int(input("Enter the lower bound for HUE: "))
+    hueUpper = int(input("Enter the upper bound for HUE: "))
 else:
-    # (rLower, gLower, bLower) = [90, 95, 10]
-    # (rUpper, gUpper, bUpper) = [150, 150, 40]
     hueLower = 30
     hueUpper = 70
 
@@ -20,29 +16,28 @@ while ret:
     frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(frame)
 
-    # Filter the image to prepare the mask
-    # res, threshR = cv2.threshold(r, rLower, rUpper, cv2.THRESH_TOZERO)
-    # res, threshG = cv2.threshold(g, gLower, gUpper, cv2.THRESH_TOZERO)
-    # res, threshB = cv2.threshold(b, bLower, bUpper, cv2.THRESH_TOZERO)
-
+    # Find all the pixels that have the HSV value be in range.
+    # Notice: All the ranges needs to be satisfied, not only one of H,S or V.
     mask = cv2.inRange(frame, np.array([hueLower, 100, 100]), np.array([hueUpper, 255, 255]))
     mask = cv2.merge((mask, mask, mask))
 
-    print(frame[360][640])
-
+    # Using the mask to mask out all the out of range pixel
     filtered_frame = cv2.bitwise_and(frame, mask)
 
-    # gray_image = cv2.cvtColor(filtered_frame, cv2.COLOR_HSV2GRAY)
     hueNew, Snew, gray_image = cv2.split(filtered_frame)
 
+    # Enhanced the details to make finding contours easier.
     dilated_kernel = (100, 100)
     dilated_image = cv2.dilate(gray_image, dilated_kernel, iterations=10)
     erode_kernel = (5,5)
     eroded_image = cv2.erode(dilated_image, erode_kernel, iterations=10)
 
+    # Find all the contours. Using cv2.RETR_EXTERNAL because we are only interested in
+    # the outermost contours, i.e., the contours bound a whole object.
     image, contours, hier = cv2.findContours(
         eroded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Draw the bounding rectanle
     for contour in contours:
         (x, y, width, height) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y),
